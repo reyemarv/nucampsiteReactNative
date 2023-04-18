debf1dfde805f01e1ebc0eafbf4ca12556bb8665
Marvin Reyes <reyemarv@gmail.com>
2023-04-17 23:12:43 -0500

    Picking an Image

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,8 +1,11 @@
 import { useEffect, useState } from 'react';
-import { View, ScrollView, StyleSheet } from 'react-native';
+import { View, ScrollView, StyleSheet, Image } from 'react-native';
 import { CheckBox, Input, Button, Icon } from 'react-native-elements';
 import * as SecureStore from 'expo-secure-store';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import * as ImagePicker from 'expo-image-picker';
+import { baseUrl } from '../shared/baseUrl';
+import logo from '../assets/images/logo.png';
 
 const LoginTab = ({ navigation }) => {
     const [username, setUsername] = useState('');
@@ -106,6 +109,7 @@ const RegisterTab = () => {
     const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
     const [remember, setRemember] = useState(false);
+    const [imageUrl, setImageUrl] = useState(baseUrl + 'images/logo.png');
 
     const handleRegister = () => {
         const userInfo = {
@@ -129,9 +133,32 @@ const RegisterTab = () => {
             SecureStore.deleteItemAsync('userinfo').catch((error) => console.log('Could not delete user info', error));
         }
     };
+   
+    const getImageFromCamera = async () => {
+        const cameraPermission = await ImagePicker.requestCameraPermissionsAsync();
+
+        if (cameraPermission.status === 'granted') {
+            const capturedImage = await ImagePicker.launchCameraAsync({
+                allowsEditing: true,
+                aspect: [1, 1]
+            });
+            if (capturedImage.assets) {
+                console.log(capturedImage.assets[0]);
+                setImageUrl(capturedImage.assets[0].uri);
+            }
+        }
+    };
 
     return (
         <ScrollView>
+            <View style={styles.imageContainer}>
+                <Image
+                    source={{ uri: imageUrl }}
+                    loadingIndicatorSource={logo}
+                    style={styles.image}
+                />
+                <Button title='Camera' onPress={getImageFromCamera} />
+            </View>
             <View style={styles.container}>
                 <Input
                     Placeholder='Username'
@@ -198,7 +225,7 @@ const RegisterTab = () => {
                 </View>
             </View>
         </ScrollView>
-    )
+    );
 };
 
 const Tab = createBottomTabNavigator();
@@ -268,6 +295,17 @@ const styles = StyleSheet.create({
         margin: 20,
         marginRight: 40,
         marginLeft: 40
+    },
+    imageContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        margin: 10
+    },
+    image: {
+        width: 60,
+        height: 60
     }
 });
 
